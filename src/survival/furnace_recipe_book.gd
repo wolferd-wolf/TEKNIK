14520a5306bc1bde85c0612adc582dcb3526e03b
wolferd-wolf <akilawolfs@gmail.com>
2026-07-28 19:30:44 +0530

    Support furnace recipe selection and alloying

--- src/survival/furnace_recipe_book.gd
+++ src/survival/furnace_recipe_book.gd
@@ -2,50 +2,92 @@ class_name TeknikFurnaceRecipeBook
 extends RefCounted
 
 const ItemRegistry = preload("res://src/survival/item_registry.gd")
+const RecipeBook = preload("res://src/survival/recipe_book.gd")
 const StackInventory = preload("res://src/survival/stack_inventory.gd")
 
-const RECIPES: Array[Dictionary] = [
-	{"input": ItemRegistry.ITEM_ZINC_CONCENTRATE, "output": ItemRegistry.ITEM_ZINC_INGOT},
-	{"input": ItemRegistry.ITEM_COPPER_CONCENTRATE, "output": ItemRegistry.ITEM_COPPER_INGOT},
-	{"input": ItemRegistry.ITEM_IRON_CONCENTRATE, "output": ItemRegistry.ITEM_IRON_INGOT},
-	{"input": ItemRegistry.ITEM_GOLD_CONCENTRATE, "output": ItemRegistry.ITEM_GOLD_INGOT},
-]
+const FUEL_ITEM: StringName = ItemRegistry.ITEM_WOOD
+const FUEL_PER_OPERATION: int = 1
+
+
+static func recipe_ids() -> Array[StringName]:
+	return RecipeBook.station_recipes(RecipeBook.STATION_FURNACE)
 
 
 static func recipes() -> Array[Dictionary]:
-	return RECIPES.duplicate(true)
+	var result: Array[Dictionary] = []
+	for recipe_id: StringName in recipe_ids():
+		result.append(RecipeBook.recipe(recipe_id))
+	return result
 
 
-static func first_available(inventory: TeknikStackInventory) -> Dictionary:
-	if inventory.count(ItemRegistry.ITEM_WOOD) < 1:
-		return {}
-	for definition: Dictionary in RECIPES:
-		var input_item := StringName(str(definition.input))
-		if inventory.count(input_item) > 0:
-			return definition.duplicate(true)
+static func can_smelt(
+	inventory: TeknikStackInventory,
+	recipe_id: StringName,
+	progression: TeknikProgressionState = null
+) -> bool:
+	if inventory.count(FUEL_ITEM) < FUEL_PER_OPERATION:
+		return false
+	return RecipeBook.can_craft_at_station(
+		inventory,
+		recipe_id,
+		progression,
+		RecipeBook.STATION_FURNACE
+	)
+
+
+static func first_available(
+	inventory: TeknikStackInventory,
+	progression: TeknikProgressionState = null
+) -> Dictionary:
+	for recipe_id: StringName in recipe_ids():
+		if can_smelt(inventory, recipe_id, progression):
+			return RecipeBook.recipe(recipe_id)
 	return {}
 
 
-static func smelt_one(inventory: TeknikStackInventory) -> Dictionary:
-	var definition: Dictionary = first_available(inventory)
-	if definition.is_empty():
+static func smelt(
+	inventory: TeknikStackInventory,
+	recipe_id: StringName,
+	progression: TeknikProgressionState = null
+) -> Dictionary:
+	if not can_smelt(inventory, recipe_id, progression):
 		return {}
-	var input_item := StringName(str(definition.input))
-	var output_item := StringName(str(definition.output))
+	var definition: Dictionary = RecipeBook.recipe(recipe_id)
 	var trial := StackInventory.new()
 	if not trial.decode(inventory.encode()):
 		return {}
-	if not trial.remove(ItemRegistry.ITEM_WOOD, 1):
+	if not trial.remove(FUEL_ITEM, FUEL_PER_OPERATION):
 		return {}
-	if not trial.remove(input_item, 1):
-		return {}
-	if trial.add(output_item, 1) != 0:
+	for item_variant: Variant in (definition.ingredients as Dictionary).keys():
+		var item_id := StringName(str(item_variant))
+		if not trial.remove(item_id, int(definition.ingredients[item_variant])):
+			return {}
+	var output_item := StringName(str(definition.output_item))
+	var output_count: int = int(definition.output_count)
+	if trial.add(output_item, output_count) != 0:
 		return {}
 	if not inventory.decode(trial.encode()):
 		return {}
+	if progression != null:
+		var granted := StringName(str(definition.get("grants_unlock", "")))
+		if granted != &"":
+			progression.unlock(granted)
 	return {
-		"fuel": ItemRegistry.ITEM_WOOD,
-		"input": input_item,
+		"recipe": recipe_id,
+		"fuel": FUEL_ITEM,
+		"fuel_count": FUEL_PER_OPERATION,
+		"ingredients": definition.ingredients,
 		"output": output_item,
-		"count": 1,
+		"count": output_count,
+		"source_process": str(definition.get("source_process", "Smelting")),
 	}
+
+
+static func smelt_one(
+	inventory: TeknikStackInventory,
+	progression: TeknikProgressionState = null
+) -> Dictionary:
+	var definition: Dictionary = first_available(inventory, progression)
+	if definition.is_empty():
+		return {}
+	return smelt(inventory, StringName(str(definition.id)), progression)
